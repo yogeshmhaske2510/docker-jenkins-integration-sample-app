7efb5760bc60f3b9e320bfd509bbe8715a15e81d
Yogesh Mhaske <yogeshmhaske2510@gmail.com>
2024-03-02 12:16:42 +0530

    sample code for docker and jenkins integration

---
+++ src/main/java/com/example/springbootcicdusingjenkins/SpringBootCicdUsingJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdusingjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCicdUsingJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCicdUsingJenkinsApplication.class, args);
+	}
+
+}
