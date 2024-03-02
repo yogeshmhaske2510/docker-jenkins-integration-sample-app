4172f86f4033f3598bdee6aa25c8707d11c3214a
Yogesh Mhaske <yogeshmhaske2510@gmail.com>
2024-03-02 12:46:41 +0530

    Test commit

--- src/main/java/com/example/springbootcicdusingjenkins/SpringBootCicdUsingJenkinsApplication.java
+++ src/main/java/com/example/springbootcicdusingjenkins/SpringBootCicdUsingJenkinsApplication.java
@@ -2,12 +2,20 @@ package com.example.springbootcicdusingjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootCicdUsingJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCicdUsingJenkinsApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Going in right direction!!!";
 	}
 
 }
